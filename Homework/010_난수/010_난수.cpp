efc65d15aac35155a0fdec26aebcad454ff10f7e
PPUDANI <zxvk1030@naver.com>
2023-03-21 15:49:01 +0900

    이현조: 전역변수,(if,for차이),클래스,과제

---
+++ Homework/010_난수/010_난수.cpp
@@ -0,0 +1,26 @@
+﻿// 010_난수.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 두번째 문제 
+
+#include <iostream>
+
+int main()
+{
+
+}
+
+int StringToInt(const char* _String)
+{
+    int nResult = 0;
+    int nIndex = 0;
+
+    // && 뒤쪽은 예외처리 (숫자일때만) 
+    while (_String[nIndex] != '\0' && (_Sting[nIndex]) >= '0' && _String[nIndex] <= '9'))
+
+    {
+        char Value ... // 5번
+    }
+}
+
+
+
+
