3a48309a7a197875718e1d6e74c038553e484198
hyeonjo <gogreen97@naver.com>
2023-04-04 11:55:58 +0900

    이현조: 상속-생성자와 소멸자_가상화 함수까지 0404

---
+++ Homework/(A)HomeWork0330/Monster.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ConsoleGameObject.h"
+
+class Monster : public ConsoleGameObject
+{
+public:
+	Monster();
+	void Update();
+
+	int2 GetNextPos() 
+	{
+		return { Pos.X + Dir, Pos.Y };
+	}
+
+	void Down()
+	{
+		Pos.Y += 1;
+		Dir *= -1;
+		DownCheck = true;
+	}
+
+private:
+	bool DownCheck = false;
+	int Dir = 1;
+};
+
